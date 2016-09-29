b5fcf1709accabd6ef5487764df697aa3fdb0605
coreyderosa <coreyderosa@gmail.com>
2016-09-28 18:48:38 -0600

    Initial commit
    Added Item 43 script to new repository

---
+++ Python Course- Item 43 of 63.py
@@ -0,0 +1,12 @@
+#Python Course- Item 43 of 62
+# First Drill
+for i in range(4):
+    print i
+
+#Second Drill
+for i in range(3, -1,-1):
+    print i
+
+#Third Drill
+for i in range(8, 0, -2):
+    print i
