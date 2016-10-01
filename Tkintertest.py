bb4e100aa089c20eaf2312aa5a8360c14d3a8cae
coreyderosa <coreyderosa@gmail.com>
2016-09-30 21:32:48 -0600

    TKinter
    Testing TKinter module

--- Tkintertest.py
+++ Tkintertest.py
@@ -1,5 +1,34 @@
+'''
 from Tkinter import *
 root = Tk()
 w = Label(root, text = "Hello, World!")
 w.pack()
 root.mainloop()
+'''
+
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text = "QUIT", fg = "red", command = frame.quit #fg is short for foreground
+            )
+        self.button.pack(side = LEFT)
+
+        self.hi_there = Button(frame, text = "Hello", command = self.say_hi)
+        self.hi_there.pack(side = LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+root.destroy()
